87ed236f216860a6250303b2d7c2aaac6a79b51d
Andis Blukis <Blukis@aab-mbp2.gateway.2wire.net>
2016-02-03 21:53:05 -0800

    formatting

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-var fs = require("fs");
-var Handlebars = require("handlebars");
+var fs = require('fs');
+var gravatar = require('gravatar');
+var _ = require('lodash')
+var Mustache = require('mustache');
 
-function render( resume ) {
-
-	var css = fs.readFileSync( __dirname + "/style.css", "utf-8" );
-	var tpl = fs.readFileSync( __dirname + "/resume.hbs", "utf-8" );
-
-	return Handlebars.compile( tpl )({
-
-		css: css,
-		resume: resume
-
+function render(resumeObject) {
+	_.each(resumeObject.work, function(w){
+		w.startDateYear = w.startDate.substr(0,4);
+		if(w.endDate) {
+			w.endDateYear = w.endDate.substr(0,4);
+		} else {
+			w.endDateYear = 'Present'
+		}
+	});
+	_.each(resumeObject.volunteer, function(v){
+		v.startDateYear = v.startDate.substr(0,4);
+		if(v.endDate) {
+			v.endDateYear = v.endDate.substr(0,4);
+		} else {
+			v.endDateYear = 'Present'
+		}
+	});
+	_.each(resumeObject.education, function(e){
+		if( !e.area || !e.studyType ){
+			e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+		}  else {
+			e.educationDetail = e.area + " ("+ e.studyType + ")";
+		}
+		e.startDateYear = e.startDate.substr(0,4);
+		if(e.endDate) {
+			e.endDateYear = e.endDate.substr(0,4);
+		}  else {
+			e.endDateYear = 'Present'
+		}
 	});
 
-}
+	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
+	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	resumeObject.css = css;
+	var resumeHTML = Mustache.render(theme, resumeObject);
+
+	return resumeHTML;
+};
 
 module.exports = {
 	render: render
-};
+}
