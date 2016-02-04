84d38bb4548e47605c449bebcfa40555d702c4d3
Andis A. Blukis <andis.blukis@rr.lv>
2016-02-03 22:50:01 -0800

    template changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var fs = require("fs");
-var _ = require('lodash')
 var Handlebars = require("handlebars");
 
 function render(resume) {
@@ -8,35 +7,45 @@ function render(resume) {
 			resume.basics.profiles[i].class = resume.basics.profiles[i].network.toLowerCase();
 		}
 	}
-	_.each(resumeObject.work, function(w){
-		w.startDateYear = w.startDate.substr(0,4);
-		if(w.endDate) {
-			w.endDateYear = w.endDate.substr(0,4);
-		} else {
-			w.endDateYear = 'Present'
+	if (resume.work && resume.work.length > 0) {
+		for(var i=0; i<resume.work.length; i++) {
+			w = work[i];
+			w.startDateYear = w.startDate.substr(0,4);
+			if(w.endDate) {
+				w.endDateYear = w.endDate.substr(0,4);
+			} else {
+				w.endDateYear = 'Present'
+			}
 		}
-	});
-	_.each(resumeObject.volunteer, function(v){
-		v.startDateYear = v.startDate.substr(0,4);
-		if(v.endDate) {
-			v.endDateYear = v.endDate.substr(0,4);
-		} else {
-			v.endDateYear = 'Present'
+	}
+	if (resume.volunteer && resume.volunteer.length > 0) {
+		for(var i=0; i<resume.volunteer.length; i++) {
+			w = volunteer[i];
+			w.startDateYear = w.startDate.substr(0,4);
+			if(w.endDate) {
+				w.endDateYear = w.endDate.substr(0,4);
+			} else {
+				w.endDateYear = 'Present'
+			}
 		}
-	});
-	_.each(resumeObject.education, function(e){
-		if( !e.area || !e.studyType ){
-			e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
-		}  else {
-			e.educationDetail = e.area + " ("+ e.studyType + ")";
+
+	}
+	if (resume.education && resume.education.length > 0) {
+		for(var i=0; i<resume.education.length; i++) {
+			e = education[i];
+			if( !e.area || !e.studyType ){
+				e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+			}  else {
+				e.educationDetail = e.area + " ("+ e.studyType + ")";
+			}
+			e.startDateYear = e.startDate.substr(0,4);
+			if(e.endDate) {
+				e.endDateYear = e.endDate.substr(0,4);
+			}  else {
+				e.endDateYear = 'Present'
+			}
 		}
-		e.startDateYear = e.startDate.substr(0,4);
-		if(e.endDate) {
-			e.endDateYear = e.endDate.substr(0,4);
-		}  else {
-			e.endDateYear = 'Present'
-		}
-	});
+	}
 
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
